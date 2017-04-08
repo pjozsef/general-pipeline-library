18e7f3670dd39d111599eecaeed2b47730dc1f39
jpollak <jozsef.pollak@emarsys.com>
2017-04-09 00:32:06 +0200

    fix(with-slack): fix stackoverflow

--- vars/withSlack.groovy
+++ vars/withSlack.groovy
@@ -1,17 +1,19 @@
 def call(Map args, Closure body) {
     def prev = currentBuild.rawBuild.previousBuild?.result
-    def name = env.JOB_NAME + " " + env.BUILD_DISPLAY_NAME
     def text
     def attachments = []
     try {
         body()
 
         if (prev && prev == hudson.model.Result.FAILURE) {
-            text = "$name is back to stable!"
+            text = "$env.JOB_NAME is back to stable!"
             attachments << [fallback: text,
-                            color   : "good"]
+                            color   : "good",
+                            text: "Yay! \\(^_^)/",
+                            thumb_url: 'https://itisatechiesworld.files.wordpress.com/2015/01/cool-jenkins2x3.png']
         }
     } catch (e) {
+        def name = env.JOB_NAME + " " + env.BUILD_DISPLAY_NAME
         def classic = env.BUILD_URL
         def console = "${env.BUILD_URL}console"
         def blueocean = "${env.JENKINS_URL}blue/organizations/jenkins/${env.JOB_NAME}/detail/${env.JOB_NAME}/${env.BUILD_NUMBER}/pipeline"
@@ -32,13 +34,14 @@ def call(Map args, Closure body) {
         }
         if(recursiveCause(e)){
             fields << [title: "Error cause",
-                       value: recursiveCause(e).class,
+                       value: recursiveCause(e),
                        short: false]
         }
         text = (prev && prev == hudson.model.Result.FAILURE) ? "$name is still failing" : "$name is failing"
         attachments << [fallback: text,
                         color   : "danger",
-                        fields  : fields]
+                        fields  : fields,
+                        thumb_url: 'https://www.cloudbees.com/sites/default/files/blog/butler-devil.png']
         currentBuild.result = "FAILED"
         throw e
     } finally {
@@ -55,6 +58,5 @@ private recursiveCause(e){
     while(cause?.cause){
         cause = cause.cause
     }
-    return (cause ?: e)
+    return (cause ?: e)?.class?.toString()
 }
-
