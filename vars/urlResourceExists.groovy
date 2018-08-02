21febf95de816fa186e2f56683de52e1de36de9f
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-08-02 19:47:58 +0200

    feat(url-resource): make mavenArtifactExists more generic

--- vars/urlResourceExists.groovy
+++ vars/urlResourceExists.groovy
@@ -1,15 +1,16 @@
 def call(Map args) {
     def url = args['forUrl']
+    def acceptableStatusCodes = args['withAcceptableStatusCodes'] || ['200']
     Closure doIfExists = args['doIfExists'] as Closure
     Closure doIfDoesntExist = args['doIfDoesntExist'] as Closure
 
     def statusCode = sh returnStdout: true, script: "curl -I $url | head -n 1 | cut -d ' ' -f2"
-    def releaseExists = "200" == statusCode.trim()
-    if (releaseExists) {
+    def resourceExists = statusCode.trim() in acceptableStatusCodes
+    if (resourceExists) {
         doIfExists?.call()
     } else {
         doIfDoesntExist?.call()
     }
 
-    return releaseExists
+    return resourceExists
 }
